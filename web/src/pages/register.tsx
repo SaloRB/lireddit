bc9fede3ee8719b622df174efda2812b28c1d1c7
Salomon Romano <salo_romano@yahoo.com>
2022-06-09 12:01:08 -0500

    Front and Back Merge

---
+++ web/src/pages/register.tsx
@@ -0,0 +1,59 @@
+import { Box, Button } from '@chakra-ui/react'
+import { Formik, Form } from 'formik'
+import { useRouter } from 'next/router'
+import { InputField } from '../components/InputField'
+import { Wrapper } from '../components/Wrapper'
+import { useRegisterMutation } from '../generated/graphql'
+import { toErrorMap } from '../utils/toErrorMap'
+
+interface registerProps {}
+
+const Register: React.FC<registerProps> = ({}) => {
+  const router = useRouter()
+  const [, register] = useRegisterMutation()
+
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: '', password: '' }}
+        onSubmit={async (values, { setErrors }) => {
+          const response = await register(values)
+
+          if (response.data?.register.errors) {
+            setErrors(toErrorMap(response.data.register.errors))
+          } else if (response.data?.register.user) {
+            router.push('/')
+          }
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="username"
+              label="Username"
+              placeholder="username"
+            />
+            <Box mt={4}>
+              <InputField
+                name="password"
+                label="Password"
+                placeholder="password"
+                type="password"
+              />
+            </Box>
+            <Button
+              mt={4}
+              type="submit"
+              isLoading={isSubmitting}
+              colorScheme="teal"
+            >
+              Register
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  )
+}
+
+export default Register

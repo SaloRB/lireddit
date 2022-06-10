797e827bd6ae052e02506447198b89b5d82b8061
Salomon Romano <salo_romano@yahoo.com>
2022-06-10 13:20:25 -0500

    TypeORM Setup

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -1,59 +1,67 @@
-import { RequiredEntityData } from '@mikro-orm/core'
-import { Arg, Ctx, Mutation, Query, Resolver } from 'type-graphql'
-import { Post } from '../entities/Post'
+import { isAuth } from '../middleware/isAuth'
 import { MyContext } from '../types'
+import {
+  Arg,
+  Ctx,
+  Field,
+  InputType,
+  Mutation,
+  Query,
+  Resolver,
+  UseMiddleware,
+} from 'type-graphql'
+import { Post } from '../entities/Post'
+
+@InputType()
+class PostInput {
+  @Field()
+  title: string
+
+  @Field()
+  text: string
+}
 
 @Resolver()
 export class PostResolver {
   @Query(() => [Post])
-  posts(@Ctx() { em }: MyContext): Promise<Post[]> {
-    return em.find(Post, {})
+  posts(): Promise<Post[]> {
+    return Post.find()
   }
 
   @Query(() => Post, { nullable: true })
-  post(@Arg('id') _id: number, @Ctx() { em }: MyContext): Promise<Post | null> {
-    return em.findOne(Post, { _id })
+  post(@Arg('id') _id: number): Promise<Post | undefined> {
+    return Post.findOne(_id)
   }
 
   @Mutation(() => Post)
+  @UseMiddleware(isAuth)
   async createPost(
-    @Arg('title') title: string,
-    @Ctx() { em }: MyContext
+    @Arg('input') input: PostInput,
+    @Ctx() { req }: MyContext
   ): Promise<Post | null> {
-    const post = em.create(Post, { title } as RequiredEntityData<Post>)
-    await em.persistAndFlush(post)
-    return post
+    return Post.create({ ...input, creatorId: req.session.userId }).save()
   }
 
   @Mutation(() => Post, { nullable: true })
   async updatePost(
     @Arg('id') _id: number,
-    @Arg('title', () => String, { nullable: true }) title: string,
-    @Ctx() { em }: MyContext
+    @Arg('title', () => String, { nullable: true }) title: string
   ): Promise<Post | null> {
-    const post = await em.findOne(Post, { _id })
+    const post = await Post.findOne(_id)
     if (!post) {
       return null
     }
 
     if (typeof title !== 'undefined') {
-      post.title = title
-      await em.persistAndFlush(post)
+      await Post.update({ _id }, { title })
     }
 
     return post
   }
 
   @Mutation(() => Boolean)
-  async deletePost(
-    @Arg('id') _id: number,
-    @Ctx() { em }: MyContext
-  ): Promise<boolean> {
-    try {
-      await em.nativeDelete(Post, { _id })
-      return true
-    } catch (err) {
-      return false
-    }
+  async deletePost(@Arg('id') _id: number): Promise<boolean> {
+    await Post.delete(_id)
+    return true
   }
 }
--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -1,4 +1,3 @@
-import { EntityManager } from '@mikro-orm/postgresql'
 import argon2 from 'argon2'
 import {
   Arg,
@@ -9,6 +8,7 @@ import {
   Query,
   Resolver,
 } from 'type-graphql'
+import { getConnection } from 'typeorm'
 import { v4 } from 'uuid'
 import { COOKIE_NAME, FORGET_PASSWORD_PREFIX } from '../constants'
 import { User } from '../entities/User'
@@ -41,7 +41,7 @@ export class UserResolver {
   async changePassword(
     @Arg('token') token: string,
     @Arg('newPassword') newPassword: string,
-    @Ctx() { redis, em, req }: MyContext
+    @Ctx() { redis, req }: MyContext
   ): Promise<UserResponse> {
     if (newPassword.length <= 2) {
       return {
@@ -69,7 +69,8 @@ export class UserResolver {
       }
     }
 
-    const user = await em.findOne(User, { _id: parseInt(userId) })
+    const userIdNum = parseInt(userId)
+    const user = await User.findOne(userIdNum)
 
     if (!user) {
       return {
@@ -82,8 +83,10 @@ export class UserResolver {
       }
     }
 
-    user.password = await argon2.hash(newPassword)
-    await em.persistAndFlush(user)
+    await User.update(
+      { _id: userIdNum },
+      { password: await argon2.hash(newPassword) }
+    )
 
     await redis.del(key)
 
@@ -96,9 +99,9 @@ export class UserResolver {
   @Mutation(() => Boolean)
   async forgotPassword(
     @Arg('email') email: string,
-    @Ctx() { em, redis }: MyContext
+    @Ctx() { redis }: MyContext
   ) {
-    const user = await em.findOne(User, { email })
+    const user = await User.findOne({ where: { email } })
     if (!user) {
       // the email is not in the db
       return true
@@ -123,19 +126,18 @@ export class UserResolver {
   }
 
   @Query(() => User, { nullable: true })
-  async me(@Ctx() { em, req }: MyContext) {
+  me(@Ctx() { req }: MyContext) {
     if (!req.session.userId) {
       return null
     }
 
-    const user = em.findOne(User, { _id: req.session.userId })
-    return user
+    return User.findOne(req.session.userId)
   }
 
   @Mutation(() => UserResponse)
   async register(
     @Arg('options') options: UsernamePasswordInput,
-    @Ctx() { em, req }: MyContext
+    @Ctx() { req }: MyContext
   ): Promise<UserResponse> {
     const errors = validateRegister(options)
 
@@ -148,19 +150,19 @@ export class UserResolver {
     let user
 
     try {
-      const result = await (em as EntityManager)
-        .createQueryBuilder(User)
-        .getKnexQuery()
-        .insert({
+      const result = await getConnection()
+        .createQueryBuilder()
+        .insert()
+        .into(User)
+        .values({
           username: options.username,
           email: options.email,
           password: hashedPassword,
-          created_at: new Date(),
-          updated_at: new Date(),
         })
         .returning('*')
+        .execute()
 
-      user = result[0]
+      user = result.raw[0]
     } catch (err) {
       if (err.code === '23505') {
         return {
@@ -186,13 +188,12 @@ export class UserResolver {
   async login(
     @Arg('usernameOrEmail') usernameOrEmail: string,
     @Arg('password') password: string,
-    @Ctx() { em, req }: MyContext
+    @Ctx() { req }: MyContext
   ): Promise<UserResponse> {
-    const user = await em.findOne(
-      User,
+    const user = await User.findOne(
       usernameOrEmail.includes('@')
-        ? { email: usernameOrEmail }
-        : { username: usernameOrEmail }
+        ? { where: { email: usernameOrEmail } }
+        : { where: { username: usernameOrEmail } }
     )
 
     if (!user) {

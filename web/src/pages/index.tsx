dc2233d04964995afc6a4210c8350545726a47a0
Salomon Romano <salo_romano@yahoo.com>
2022-06-13 17:55:38 -0500

    Upvote and Downvote functionallity

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -2,9 +2,20 @@ import { usePostsQuery } from '../generated/graphql'
 import { createUrqlClient } from '../utils/createUrqlClient'
 import { withUrqlClient } from 'next-urql'
 import { Layout } from '../components/Layout'
-import { Box, Button, Flex, Heading, Link, Stack, Text } from '@chakra-ui/react'
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  IconButton,
+  Link,
+  Stack,
+  Text,
+} from '@chakra-ui/react'
 import NextLink from 'next/link'
 import { useState } from 'react'
+import { ChevronDownIcon, ChevronUpIcon } from '@chakra-ui/icons'
+import { UpdootSection } from '../components/UpdootSection'
 
 const Index = () => {
   const [variables, setVariables] = useState({
@@ -34,11 +45,14 @@ const Index = () => {
       ) : (
         <Stack spacing={8}>
           {data!.posts.posts.map((p) => (
-            <Box key={p._id} p={5} shadow="md" borderWidth="1px">
-              <Heading fontSize="xl">{p.title}</Heading>{' '}
-              <Text>post by {p.creator.username}</Text>
-              <Text mt={4}>{p.textSnippet}</Text>
-            </Box>
+            <Flex key={p._id} p={5} shadow="md" borderWidth="1px">
+              <UpdootSection post={p} />
+              <Box>
+                <Heading fontSize="xl">{p.title}</Heading>
+                <Text>post by {p.creator.username}</Text>
+                <Text mt={4}>{p.textSnippet}</Text>
+              </Box>
+            </Flex>
           ))}
         </Stack>
       )}

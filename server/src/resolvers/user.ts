607d48d4cd9bb8686c20c49d4f4995029a6da44e
Salomon Romano <salo_romano@yahoo.com>
2022-06-13 14:49:03 -0500

    Field permission on email

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -3,10 +3,12 @@ import {
   Arg,
   Ctx,
   Field,
+  FieldResolver,
   Mutation,
   ObjectType,
   Query,
   Resolver,
+  Root,
 } from 'type-graphql'
 import { getConnection } from 'typeorm'
 import { v4 } from 'uuid'
@@ -35,8 +37,19 @@ class UserResponse {
   user?: User
 }
 
-@Resolver()
+@Resolver(User)
 export class UserResolver {
+  @FieldResolver(() => String)
+  email(@Root() user: User, @Ctx() { req }: MyContext) {
+    // this is the current user and its ok to show them their own email
+    if (req.session.userId === user._id) {
+      return user.email
+    }
+
+    // current user want to see someone elses email
+    return ''
+  }
+
   @Mutation(() => UserResponse)
   async changePassword(
     @Arg('token') token: string,

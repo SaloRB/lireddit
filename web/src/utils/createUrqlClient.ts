f5b89137ee35b94e83fe9ea016e4c8d264933967
Salomon Romano <salo_romano@yahoo.com>
2022-06-13 20:39:53 -0500

    Vote Fragment

--- web/src/utils/createUrqlClient.ts
+++ web/src/utils/createUrqlClient.ts
@@ -1,16 +1,22 @@
 import { cacheExchange, Resolver } from '@urql/exchange-graphcache'
-import { dedupExchange, fetchExchange, stringifyVariables } from 'urql'
-import { Exchange } from 'urql'
+import gql from 'graphql-tag'
+import Router from 'next/router'
+import {
+  dedupExchange,
+  Exchange,
+  fetchExchange,
+  stringifyVariables,
+} from 'urql'
+import { pipe, tap } from 'wonka'
 import {
   LoginMutation,
   LogoutMutation,
   MeDocument,
   MeQuery,
   RegisterMutation,
+  VoteMutationVariables,
 } from '../generated/graphql'
 import { betterUpdateQuery } from './betterUpdateQuery'
-import { pipe, tap } from 'wonka'
-import Router from 'next/router'
 
 const errorExchange: Exchange =
   ({ forward }) =>
@@ -84,6 +90,31 @@ export const createUrqlClient = (ssrEchange: any) => ({
       },
       updates: {
         Mutation: {
+          vote: (_result, args, cache, info) => {
+            const { postId, value } = args as VoteMutationVariables
+
+            const data = cache.readFragment(
+              gql`
+                fragment _ on Post {
+                  _id
+                  points
+                }
+              `,
+              { _id: postId } as any
+            )
+
+            if (data) {
+              const newPoints = (data.points as number) + value
+              cache.writeFragment(
+                gql`
+                  fragment __ on Post {
+                    points
+                  }
+                `,
+                { _id: postId, points: newPoints } as any
+              )
+            }
+          },
           createPost: (_result, args, cache, info) => {
             const allFields = cache.inspectFields('Query')
             const fieldInfos = allFields.filter(
